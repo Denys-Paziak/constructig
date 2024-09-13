e9ac473e5987fcfe57c15cd7ed803321bb602a8a
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-09-13 19:55:03 +0200

    asdasda

--- front/src/utils/axios/axios.ts
+++ front/src/utils/axios/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://185.233.117.23:3000",
-  baseURL: "http://localhost:3000",
+  baseURL: "http://185.233.117.23:3000",
+  // baseURL: "http://localhost:3000",
 });
 
 export default instance;
