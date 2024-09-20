9b0239e33f5fdfa8a25a6b60d5883e812202c735
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-09-20 12:37:15 +0200

    sad

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,5 +1,5 @@
 import express, {response} from "express";
-import {getUser, login, register, resetPass, resetPassSend, updateUser} from '../controllers/auth.js';
+import {getUser, login, register, resetPassSend, updateUser} from '../controllers/auth.js';
 import multer from "multer";
 import authMiddleware from "../middlewares/authMiddleware.js";
 const upload = multer();
@@ -13,6 +13,6 @@ router.get('/getUser', upload.none(), getUser);
 router.put("/update", upload.none(), updateUser);
 
 router.get("/reset", upload.none(), authMiddleware, resetPassSend);
-router.put("/reset2", upload.none(), authMiddleware, resetPass);
+// router.put("/reset2", upload.none(), authMiddleware, resetPass);
 
 export default router;
