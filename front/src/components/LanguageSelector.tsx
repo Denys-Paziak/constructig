3b66dcedeb01729f7f93ea6987e3989999cdcd15
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-10-11 15:13:30 +0200

    sg

--- front/src/components/LanguageSelector.tsx
+++ front/src/components/LanguageSelector.tsx
@@ -3,52 +3,14 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const LanguageSelector = ({ fetchData }) => {
-  const { i18n, t } = useTranslation();
+  const { t } = useTranslation();
 
   const changeLanguage = async (language: string) => {
-    // await i18n.changeLanguage(language); // Перемикання мови
     localStorage.setItem("siteLang", language);
     await fetchData();
   };
 
   return (
-    // <div className={"flex justify-center h-[64px]"}>
-    //   <div className="flex items-center gap-4">
-    //     {langs.map((lang) => {
-    //       if (lang === i18n.language) {
-    //         return (
-    //           <button
-    //             key={lang}
-    //             type="button"
-    //             className={
-    //               "bg-blue-800 text-white py-2 px-6 text-sm rounded hover:bg-blue-800"
-    //             }
-    //             onClick={() => {
-    //               changeLanguage(lang);
-    //             }}
-    //           >
-    //             {lang}
-    //           </button>
-    //         );
-    //       } else {
-    //         return (
-    //           <button
-    //             key={lang}
-    //             type="button"
-    //             className={
-    //               "bg-blue-600 text-white py-2 px-6 text-sm rounded hover:bg-blue-800"
-    //             }
-    //             onClick={() => {
-    //               changeLanguage(lang);
-    //             }}
-    //           >
-    //             {lang}
-    //           </button>
-    //         );
-    //       }
-    //     })}
-    //   </div>
-    // </div>
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm">
