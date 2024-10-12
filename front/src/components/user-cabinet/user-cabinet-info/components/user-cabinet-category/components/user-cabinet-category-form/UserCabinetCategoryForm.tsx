48c0c0702c49dd9fc3690e87b88bbbf9ab667c97
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-10-12 15:02:27 +0200

    asdasda

--- front/src/components/user-cabinet/user-cabinet-info/components/user-cabinet-category/components/user-cabinet-category-form/UserCabinetCategoryForm.tsx
+++ front/src/components/user-cabinet/user-cabinet-info/components/user-cabinet-category/components/user-cabinet-category-form/UserCabinetCategoryForm.tsx
@@ -8,7 +8,7 @@ import { notify, notifyError } from "../../../../../../../helpers/helper";
 import Loader from "../../../../../../loader/Loader";
 import { useTranslation } from "react-i18next";
 import Cropper from "react-easy-crop";
-import getCroppedImg from "../../../../../../../utils/cropImageUtil.ts";
+import {getCroppedImg} from "../../../../../../../utils/cropImageUtil.ts"; // Helper function for cropping
 
 interface Props {
   toggleCategoriesForm: () => void;
@@ -22,28 +22,24 @@ interface FormValues {
 }
 
 const UserCabinetCategoryForm: React.FC<Props> = ({
-  toggleCategoriesForm,
-  getAll,
-  sites,
-  fetchData,
-}) => {
+                                                    toggleCategoriesForm,
+                                                    getAll,
+                                                    sites,
+                                                    fetchData,
+                                                  }) => {
   const [mainImage, setMainImage] = useState<File | null>(null);
   const [mainImagePreview, setMainImagePreview] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadingImage, setIsLoadingImage] = useState(false);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState({});
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<any>(null);
   const [openCrop, setOpenCrop] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-    reset,
-  } = useForm<FormValues>({
+  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<FormValues>({
     mode: "onChange",
   });
+
   const { t } = useTranslation();
 
   const acceptType: Accept = {
@@ -70,29 +66,26 @@ const UserCabinetCategoryForm: React.FC<Props> = ({
     setIsLoadingImage(false);
     setMainImage(compressedFile);
     setMainImagePreview(URL.createObjectURL(compressedFile));
-    setOpenCrop(true); // Відкриваємо вікно обрізання зображення
+    setOpenCrop(true); // Open crop window
   }, []);
 
-  const onCropHandler = async () => {
-    const croppedImageBlob: any = await getCroppedImg(
-      mainImagePreview,
-      croppedAreaPixels
-    );
-    const croppedFile = new File(
-      [croppedImageBlob],
-      mainImage?.name || "cropped_image.jpg",
-      {
-        type: mainImage?.type || "image/jpeg",
+  const onCropHandler = useCallback(async () => {
+    if (!croppedAreaPixels) return;
+
+    try {
+      const croppedImage = await getCroppedImg(mainImagePreview, croppedAreaPixels);
+      setMainImage(new File([croppedImage], mainImage!.name, {
+        type: mainImage!.type,
         lastModified: Date.now(),
-      }
-    );
+      }));
+      setMainImagePreview(URL.createObjectURL(croppedImage));
+      setOpenCrop(false);
+    } catch (error) {
+      console.error("Crop failed", error);
+    }
+  }, [croppedAreaPixels, mainImagePreview, mainImage]);
 
-    setMainImage(croppedFile);
-    setMainImagePreview(URL.createObjectURL(croppedFile));
-    setOpenCrop(false);
-  };
-
-  const onCropComplete = (croppedAreaPixels: any) => {
+  const onCropComplete = (croppedArea: any, croppedAreaPixels: any) => {
     setCroppedAreaPixels(croppedAreaPixels);
   };
 
@@ -150,145 +143,120 @@ const UserCabinetCategoryForm: React.FC<Props> = ({
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="w-full flex flex-wrap gap-[20px]"
-    >
-      <div className="w-full md:w-[calc(50%-10px)] flex flex-col gap-2">
-        <label htmlFor="image" className="text-sm font-semibold">
-          {t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel1")}
-        </label>
-        <AdminImage
-          {...getMainRootProps({
-            isdragactive: isMainDragActive.toString(),
-            isdragaccept: isMainDragAccept.toString(),
-            isdragreject: isMainDragReject.toString(),
-            isfocused: isMainFocused.toString(),
-          })}
-        >
-          <input {...getMainInputProps()} />
-          {isMainDragActive ? (
-            <p>
-              {t(
-                "admin.adminInfo.adminInfoCategories.adminInfoCategoriesPlaceholder1"
-              )}
-            </p>
-          ) : (
-            <p>
-              {t(
-                "admin.adminInfo.adminInfoCategories.adminInfoCategoriesPlaceholder1"
-              )}
-            </p>
-          )}
-        </AdminImage>
-        {mainImagePreview && (
-          <>
-            <div className="w-12 h-12 mr-4">
-              <img
-                src={mainImagePreview}
-                alt="banner preview"
-                className="w-full h-full"
-              />
-            </div>
-
-            {openCrop && (
-              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-4">
-                <div className="relative w-full max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden">
-                  <div className="relative w-full h-96 sm:h-[400px] md:h-[500px] lg:h-[600px] bg-gray-100">
-                    <Cropper
-                      image={mainImagePreview}
-                      crop={crop}
-                      zoom={zoom}
-                      aspect={4 / 3}
-                      onCropChange={setCrop}
-                      onCropComplete={onCropComplete}
-                      onZoomChange={setZoom}
-                    />
-                  </div>
-                  <div className="p-4 bg-white border-t border-gray-300 flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                      <label htmlFor="zoom" className="text-gray-700">
-                        {t("cropImage.cropImageZoom")}
-                      </label>
-                      <input
-                        id="zoom"
-                        type="range"
-                        min="1"
-                        max="3"
-                        step="0.1"
-                        value={zoom}
-                        onChange={(e: any) => setZoom(e.target.value)}
-                        className="w-40"
-                      />
-                    </div>
-                    <div className="flex gap-4">
-                      <div
-                        className="px-6 py-2 text-white bg-green-600 hover:bg-green-700 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-                        onClick={onCropHandler}
-                      >
-                        {t("cropImage.cropImageButton1")}
-                      </div>
-                      <div
-                        className="px-6 py-2 text-white bg-gray-600 hover:bg-gray-700 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                        onClick={() => setOpenCrop(false)}
-                      >
-                        {t("cropImage.cropImageButton2")}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full flex flex-wrap gap-[20px]">
+        <div className="w-full md:w-[calc(50%-10px)] flex flex-col gap-2">
+          <label htmlFor="image" className="text-sm font-semibold">
+            {t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel1")}
+          </label>
+          <AdminImage
+              {...getMainRootProps({
+                isdragactive: isMainDragActive.toString(),
+                isdragaccept: isMainDragAccept.toString(),
+                isdragreject: isMainDragReject.toString(),
+                isfocused: isMainFocused.toString(),
+              })}
+          >
+            <input {...getMainInputProps()} />
+            {isMainDragActive ? (
+                <p>{t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesPlaceholder1")}</p>
+            ) : (
+                <p>{t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesPlaceholder1")}</p>
             )}
-          </>
-        )}
-      </div>
-
-      <div className="w-full md:w-[calc(50%-10px)] flex flex-col gap-2">
-        <label htmlFor="name" className="text-sm font-semibold">
-          {t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel2")}
-        </label>
-        <input
-          type="text"
-          className="py-2 px-4 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-          style={errors["name"] ? { border: "1px solid #EB001B" } : {}}
-          placeholder={t(
-            "admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel2"
+          </AdminImage>
+          {mainImagePreview && (
+              <>
+                <div className="w-12 h-12 mr-4">
+                  <img src={mainImagePreview} alt="banner preview" className="w-full h-full object-cover" />
+                </div>
+                {openCrop && (
+                    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-4">
+                      <div className="relative w-full max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden">
+                        <div className="relative w-full h-96 sm:h-[400px] md:h-[500px] lg:h-[600px] bg-gray-100">
+                          <Cropper
+                              image={mainImagePreview}
+                              crop={crop}
+                              zoom={zoom}
+                              aspect={4 / 3}
+                              onCropChange={setCrop}
+                              onCropComplete={onCropComplete}
+                              onZoomChange={setZoom}
+                          />
+                        </div>
+                        <div className="p-4 bg-white border-t border-gray-300 flex justify-between items-center">
+                          <div className="flex items-center gap-2">
+                            <label htmlFor="zoom" className="text-gray-700">
+                              {t("cropImage.cropImageZoom")}
+                            </label>
+                            <input
+                                id="zoom"
+                                type="range"
+                                min="1"
+                                max="3"
+                                step="0.1"
+                                value={zoom}
+                                onChange={(e: any) => setZoom(e.target.value)}
+                                className="w-40"
+                            />
+                          </div>
+                          <div className="flex gap-4">
+                            <div
+                                className="px-6 py-2 text-white bg-green-600 hover:bg-green-700 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                                onClick={onCropHandler}
+                            >
+                              {t("cropImage.cropImageButton1")}
+                            </div>
+                            <div
+                                className="px-6 py-2 text-white bg-gray-600 hover:bg-gray-700 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                                onClick={() => setOpenCrop(false)}
+                            >
+                              {t("cropImage.cropImageButton2")}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                )}
+              </>
           )}
-          {...register("name", { required: `Required field` })}
-        />
-        {errors["name"] && (
-          <span className="text-md text-red-500 font-light">
+        </div>
+        <div className="w-full md:w-[calc(50%-10px)] flex flex-col gap-2">
+          <label htmlFor="name" className="text-sm font-semibold">
+            {t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel2")}
+          </label>
+          <input
+              type="text"
+              className="py-2 px-4 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              style={errors["name"] ? { border: "1px solid #EB001B" } : {}}
+              placeholder={t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesLabel2")}
+              {...register("name", { required: `Required field` })}
+          />
+          {errors["name"] && (
+              <span className="text-md text-red-500 font-light">
             {errors["name"]?.message as string}
           </span>
-        )}
-      </div>
-
-      <div className="w-full flex md:flex-row flex-col gap-[20px] pt-4 border-t border-gray-300">
-        <button
-          className="w-full py-2.5 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          type="submit"
-          disabled={isLoading || !isValid}
-        >
-          {isLoading
-            ? t(
-                "admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonLoading"
-              )
-            : t(
-                "admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonConfirm"
-              )}
-        </button>
-        <button
-          onClick={toggleCategoriesForm}
-          className="w-full py-2.5 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          type="button"
-          disabled={isLoading}
-        >
-          {t(
-            "admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonCancel"
           )}
-        </button>
-      </div>
-    </form>
+        </div>
+
+        <div className="w-full flex md:flex-row flex-col gap-[20px] pt-4 border-t border-gray-300">
+          <button
+              className="w-full py-2.5 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              type="submit"
+              disabled={isLoading || !isValid}
+          >
+            {isLoading
+                ? t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonLoading")
+                : t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonConfirm")}
+          </button>
+          <button
+              onClick={toggleCategoriesForm}
+              className="w-full py-2.5 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              type="button"
+              disabled={isLoading}
+          >
+            {t("admin.adminInfo.adminInfoCategories.adminInfoCategoriesButtonCancel")}
+          </button>
+        </div>
+      </form>
   );
 };
 
--- front/src/utils/cropImageUtil.ts
+++ front/src/utils/cropImageUtil.ts
@@ -1,39 +1,47 @@
-const createImage = (url: any) =>
-  new Promise((resolve, reject) => {
-    const image = new Image();
-    image.addEventListener("load", () => resolve(image));
-    image.addEventListener("error", (error) => reject(error));
-    image.setAttribute("crossOrigin", "anonymous"); // to avoid CORS issues
-    image.src = url;
-  });
+// cropImage.ts
+export const getCroppedImg = async (imageSrc: string, pixelCrop: any) => {
+    const image = await createImage(imageSrc);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-export default async function getCroppedImg(imageSrc: any, pixelCrop: any) {
-  const image = await createImage(imageSrc);
-  const canvas = document.createElement("canvas");
-  const ctx: any | null = canvas.getContext("2d");
+    if (!ctx) {
+        throw new Error("Could not get canvas context");
+    }
 
-  canvas.width = pixelCrop.width;
-  canvas.height = pixelCrop.height;
+    // Встановлюємо розміри canvas на основі обрізаних пікселів
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
 
-  ctx!.drawImage(
-    image,
-    pixelCrop.x,
-    pixelCrop.y,
-    pixelCrop.width,
-    pixelCrop.height,
-    0,
-    0,
-    pixelCrop.width,
-    pixelCrop.height
-  );
+    // Малюємо обрізане зображення на canvas
+    ctx.drawImage(
+        image,
+        pixelCrop.x, // x позиція початку обрізання
+        pixelCrop.y, // y позиція початку обрізання
+        pixelCrop.width, // ширина обрізаного зображення
+        pixelCrop.height, // висота обрізаного зображення
+        0,
+        0,
+        pixelCrop.width,
+        pixelCrop.height
+    );
 
-  return new Promise((resolve, reject) => {
-    canvas.toBlob((blob) => {
-      if (blob) {
-        resolve(blob);
-      } else {
-        reject(new Error("Canvas is empty"));
-      }
-    }, "image/jpeg");
-  });
+    // Повертаємо canvas як Blob
+    return new Promise<Blob>((resolve, reject) => {
+        canvas.toBlob((blob) => {
+            if (!blob) {
+                reject(new Error("Canvas is empty"));
+                return;
+            }
+            resolve(blob);
+        }, "image/jpeg");
+    });
+};
+
+function createImage(url: string) {
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+        const image = new Image();
+        image.src = url;
+        image.onload = () => resolve(image);
+        image.onerror = (error) => reject(error);
+    });
 }
