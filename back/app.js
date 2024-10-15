6a2f904e22fc284fb1bb0116805ac13437153e3b
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-10-15 12:26:47 +0200

    final update

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/", siteRoutes);
 
 app.get("/api/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to Menualista API");
 });
 
 app.listen(port, () => {
